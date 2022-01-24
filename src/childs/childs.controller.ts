19c142551c4cd33b6fdc594df9f412d03b3b4657
Dzmitry Shakhrai <romantus_@mail.ru>
2022-01-25 01:41:07 +0300

    feat: deleteChild

--- src/childs/childs.controller.ts
+++ src/childs/childs.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Child, EGender } from './child.model';
 import { ChildsService } from './childs.service';
 import { CreateCheldDto } from './dto/create-child.dto';
@@ -12,11 +12,21 @@ export class ChildsController {
     return this.childsService.getChilds();
   }
 
+  @Get('/:id')
+  getChildById(@Param('id') id: string): Child {
+    return this.childsService.getChildById(id);
+  }
+
   @Post()
   createTask(
     @Body()
     createChildDto: CreateCheldDto,
   ): Child {
     return this.childsService.createChild(createChildDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.childsService.deleteChild(id);
   }
 }
--- src/childs/childs.service.ts
+++ src/childs/childs.service.ts
@@ -10,11 +10,15 @@ export class ChildsService {
     return this.childs;
   }
 
+  getChildById(id: string): Child {
+    return this.childs.find((child) => child.id === id);
+  }
+
   createChild(createChildDto: CreateCheldDto): Child {
     const { name, gender, birth, photo } = createChildDto;
 
     const child: Child = {
-      id: '',
+      id: Date.now().toString(),
       name,
       gender,
       birth,
@@ -23,5 +27,9 @@ export class ChildsService {
 
     this.childs.push(child);
     return child;
+  }
+
+  deleteChild(id: string): void {
+    this.childs = this.childs.filter((child) => child.id !== id);
   }
 }
